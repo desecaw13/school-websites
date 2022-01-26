ccece8a25c742f192fcb6bc5df7d1a4ed694a267
desecaw13 <47424966+desecaw13@users.noreply.github.com>
2022-01-26 16:56:14 -0700

    progreesd ad wdaskfh oieheafilo ehafio

--- personal/script.js
+++ personal/script.js
@@ -12,7 +12,7 @@ function evilFlip() {
 let gIn = 191; //light
 let gOut = 64; //dark
 let i = 1;
-setInterval(function () {return;
+setInterval(function () {return;//tmp
     if (gIn >= 127.5) { i = -1 } else if (gIn <= 0) { i = 1 };
     if (gOut <= 127.5) { i = -1 } else if (gOut >= 255) { i = 1 };
     document.body.style.background = `linear-gradient(rgb(${gIn},${gIn},${gIn}), rgb(${gOut},${gOut},${gOut}), rgb(${gIn},${gIn},${gIn}))`;
