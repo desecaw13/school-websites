dc9964d7fc30924bbe7ef75ea7437da044aece24
desecaw13 <47424966+desecaw13@users.noreply.github.com>
2022-02-11 12:06:50 -0700

    fin 2

--- pong/pong.js
+++ pong/pong.js
@@ -1,4 +1,4 @@
-import { Checkboxland } from './checkboxland.mjs';//'https://unpkg.com/checkboxland?module';
+import { Checkboxland } from 'https://unpkg.com/checkboxland?module';
 import Paddle from './paddle.js';
 import Ball from './ball.js';
 
